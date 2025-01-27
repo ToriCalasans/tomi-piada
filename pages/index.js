485c6cb147d33c4fb4debcd32a42ee62a3d9cda4
ToriCalasans <calasanstori@gmail.com>
2025-01-26 22:09:52 -0300

    tentando por imagem no git

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
+import David from "../src/assets/david-a-lagartixa-1.png";
 import Image from "next/image";
-
 function Home() {
   return (
     <div style={{ backgroundColor: "#157F58", height: "100vh" }}>
       <h1>Tomi Piadas</h1>
       <div>
-        <Image src="/src/assets/Davidalagartixa.png" width={500} height={500} />
+        <img src={David} alt="David a lagartixa" title="David" />
       </div>
     </div>
   );
