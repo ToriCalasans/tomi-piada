f8e273892300574f725ff9a59fcaabce3c08bb76
ToriCalasans <calasanstori@gmail.com>
2025-02-02 15:12:35 -0300

    cria página inicial com elementos básicos

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import David from "../src/assets/david-a-lagartixa-1.png";
 import Image from "next/image";
+import David from "../src/assets/david-a-lagartixa-1.png";
 function Home() {
   return (
     <div style={{ backgroundColor: "#157F58", height: "100vh" }}>
-      <h1>Tomi Piadas</h1>
       <div>
-        <img src={David} alt="David a lagartixa" title="David" />
+        <h1>Tomi Piadas</h1>
+      </div>
+      <div>
+        <Image
+          src={David}
+          alt="David a lagartixa"
+          title="David"
+          style={{ width: "300px", height: "auto" }}
+        />
+      </div>
+      <div>
+        <select name="selectTipoPiada">
+          <option value="piadaTio">Piadas de tio</option>
+          <option value="piadaTio">Piadas de tio</option>
+          <option value="piadaTio">Piadas de tio</option>
+        </select>
+      </div>
+      <div>
+        <button name="buttonPiada">Procurar piada</button>
+      </div>
+      <div>
+        <h1>Aqui fica nossa piadinha</h1>
       </div>
     </div>
   );
