5d84bc5208b0fdb8a8178d7903441df50f17d1a9
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2026-03-24 14:39:02 +0000

    feito alterações de skin

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,41 @@
 import Image from "next/image";
-import David from "../src/assets/images/david-a-lagartixa-1.png";
+import David1 from "../src/assets/images/david-a-lagartixa-1.png";
+import David2 from "../src/assets/images/david-a-lagartixa-2.png";
+import David3 from "../src/assets/images/david-a-lagartixa-3.png";
+import David4 from "../src/assets/images/david-a-lagartixa-4.png";
+import David5 from "../src/assets/images/david-a-lagartixa-5.png";
 import piadas from "../src/assets/piadas.json";
 import { useState } from "react";
 
 function Home() {
+  const imagensDavid = [David1, David2, David3, David4, David5];
+  const [indiceImagem, setIndiceImagem] = useState(0);
   const [piada, setPiada] = useState("");
   const gerarPiada = () => {
     const tipo = document.querySelector(".seletor-piada").value;
     const lista = piadas[tipo];
     const piadaAleatoria = lista[Math.floor(Math.random() * lista.length)];
     setPiada(piadaAleatoria);
+
+    let novoIndice = indiceImagem;
+    while (novoIndice === indiceImagem) {
+      novoIndice = Math.floor(Math.random() * imagensDavid.length);
+    }
+    setIndiceImagem(novoIndice);
   };
 
   return (
     <div className="container-principal">
       <div>
-        <h1>Tomi Piadas</h1>
+        <h1 className="titulo-principal">Tomi Piadas</h1>
       </div>
       <div>
-        <Image src={David} alt="David a lagartixa" title="David" />
+        <Image
+          src={imagensDavid[indiceImagem]}
+          alt="David a lagartixa"
+          title="David"
+          className="imagem-mascote"
+        />
       </div>
       <div>
         <select name="selectTipoPiada" className="seletor-piada">
@@ -37,7 +54,7 @@ function Home() {
         </button>
       </div>
       <div>
-        <h1>{piada}</h1>
+        <h1 className="texto-piada">{piada}</h1>
       </div>
     </div>
   );
